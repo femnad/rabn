e1e35f8105f79b0c16fe0f9c497c5959fb06e349
commit-person <commit-person@localhost>
2020-01-18 22:02:08 +0300

    add version

--- cmd/rabn/rabn.go
+++ cmd/rabn/rabn.go
@@ -6,13 +6,22 @@ import (
 	"github.com/femnad/rabn/pkg/rabn"
 )
 
-var args struct{
+const (
+	version = "0.1.0"
+)
+
+type args struct{
 	HistoryFile string `arg:"-H,required"`
 	PathSpec []string `arg:"-p,required,separate"`
 	Selection string `arg:"positional" default:""`
 }
 
+func (args) Version() string {
+	return version
+}
+
 func main() {
+	var args args
 	arg.MustParse(&args)
 	prefix :=  rabn.FindLongestCommonPrefix(args.PathSpec)
 	h, err := rabn.HistoryFromFile(args.HistoryFile, prefix)
